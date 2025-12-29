a897c032a861bf2fda56dac876ce324379854378
nuke <minimissile888@gmail.com>
2025-12-29 16:53:21 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AppShell } from './routes/AppShell'
 const AnalyzePage = lazy(() => import('./routes/AnalyzePage'))
 const WatchlistPage = lazy(() => import('./routes/WatchlistPage'))
 const DiscoverPage = lazy(() => import('./routes/DiscoverPage'))
+const ExportPage = lazy(() => import('./routes/ExportPage'))
 const MarketPage = lazy(() => import('./routes/MarketPage'))
 
 /** 应用根组件：提供全局状态、懒加载路由与统一布局壳。 */
@@ -21,6 +22,7 @@ function App() {
 
             <Route path="/watchlist" element={<WatchlistPage />} />
             <Route path="/discover" element={<DiscoverPage />} />
+            <Route path="/export" element={<ExportPage />} />
             <Route path="*" element={<Navigate to="/analyze" replace />} />
           </Route>
 
